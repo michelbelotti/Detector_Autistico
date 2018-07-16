5b2401e5b2cb0ff06f4de4687672c0a11b009598
michelbelotti <michelbelotti@gmail.com>
2018-07-16 19:13:54 -0300

    first commit

---
+++ Assets/timer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class timer : MonoBehaviour {
+
+    public Text timerText;
+    public bool debug = false;
+
+    private float timerCount = 0;
+
+	// Use this for initialization
+	void Start () {
+		if(!debug)
+        {
+            timerText.enabled = false;
+        }
+        else
+        {
+            timerText.enabled = true;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timerCount += Time.deltaTime;
+        timerText.text = "Timer = " + timerCount.ToString();
+	}
+}
